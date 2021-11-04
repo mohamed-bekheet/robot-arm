50c3e65fd02a14d942f4bc2c4c3b95cb26a9a2cf
m-maker2023 <86668363+m-maker2023@users.noreply.github.com>
2021-11-05 00:09:59 +0200

    add new projects

---
+++ self balancing robot/Software/fromInternet/self-balancing-robot-master/self-balancing-robot-master/include/Channel.h
@@ -0,0 +1,38 @@
+class Channel {
+  public:
+    Channel() {
+      m_uStartT = 0;
+      m_uEndT = 0;
+      m_uElapsedT = 0;
+    }
+
+    uint32_t getElapsed() {
+
+      uint32_t elapsed = m_uEndT - m_uStartT;
+      // Filter out all the nonsense
+      if (elapsed < 2100 && elapsed > 900) {
+        // Increase elapsed time slowly to reduce noise and make behaviour less aggressive
+        m_uElapsedT = (m_uElapsedT * 0.9f) + (0.1f * elapsed);
+      }
+
+      return m_uElapsedT;
+    }
+
+    inline int16_t getStickPosition() {
+      return constrain((int16_t)(1500 - getElapsed()), -500, 500);
+    }
+
+
+    inline void setStartTime(uint32_t startT) {
+      m_uStartT = startT;
+    }
+
+    inline void setEndTime(uint32_t endT) {
+      m_uEndT = endT;
+    }
+
+  private:
+    uint32_t m_uStartT;
+    uint32_t m_uEndT;
+    uint32_t m_uElapsedT;
+};
--- self balancing robot/Software/selfblancing_mega/src/AmBOT_final_nano/AmBOT_final_nano.ino
+++ self balancing robot/Software/selfblancing_mega/src/AmBOT_final_nano/AmBOT_final_nano.ino
@@ -2,10 +2,11 @@
 #include "LMotorController.h"
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
+#define LOG_INPUT
 
-#include "Wire.h"
-
-#if 1
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
 
 #define MIN_ABS_SPEED 20
 
@@ -20,17 +21,18 @@ uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 // orientation/motion vars
-Quaternion q;        // [w, x, y, z]         quaternion container
-VectorFloat gravity; // [x, y, z]            gravity vector
-float ypr[3];        // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-// PID
+
+//PID
 double originalSetpoint = 175.8;
 double setpoint = originalSetpoint;
 double movingAngleOffset = 0.1;
 double input, output;
 
-int moveState = 0; // 0 = balance; 1 = back; 2 = forth
+int moveState=0; //0 = balance; 1 = back; 2 = forth
 double Kp = 50;
 double Kd = 1.4;
 double Ki = 60;
@@ -40,63 +42,58 @@ PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
 double motorSpeedFactorLeft = 0.6;
 double motorSpeedFactorRight = 0.5;
 
-#define ENA 5
-#define IN1 17 //tx2
-#define IN2 16 //rx2
+//MOTOR CONTROLLER
+int ENA = 3;
+int IN1 = 44;
+int IN2 = 45;
 
-#define ENB 4
-#define IN3 2
-#define IN4 15
+int ENB = 4;
+int IN3 = 32;
+int IN4 = 33;
 
 LMotorController motorController(ENA, IN1, IN2, ENB, IN3, IN4, motorSpeedFactorLeft, motorSpeedFactorRight);
 
-// timers
+//timers
 long time1Hz = 0;
 long time5Hz = 0;
 
-volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
-
-void IRAM_ATTR dmpDataReady()
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady()
 {
     mpuInterrupt = true;
 }
 
+
 void setup()
 {
 
-#if 0
-    pinMode(IN1,OUTPUT);
-    pinMode(IN2,OUTPUT);
-    pinMode(IN3,OUTPUT);
-    pinMode(IN4,OUTPUT);
+  Serial.begin(115200);
+  Serial.println("hi");
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
 
-#endif
-    pinMode(18, INPUT_PULLDOWN);
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    
+    //while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
-#if 0
-    digitalWrite(IN1,HIGH);
-    digitalWrite(IN2,LOW);
-    digitalWrite(IN3,HIGH);
-    digitalWrite(IN4,LOW);
-    ledcWrite(ENA_ch,255);
-    ledcWrite(ENB_ch,255);
-#endif
-
-    Wire.begin();
-    Serial.begin(115200);
-    Serial.println(F("Initializing I2C devices..."));
+    // initialize device
+    Serial.println("Initializing I2C devices...");
     mpu.initialize();
-    Serial.println(F("Testing device connections..."));
+
+    // verify connection
+    Serial.println(("Testing device connections..."));
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-    Serial.println(F("Initializing DMP..."));
+
+    // load and configure the DMP
+    Serial.println(("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
-#if 0
-    // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(137);  // 220//[136,137] --> [0,2]
-    mpu.setYGyroOffset(-41);  // 76
-    mpu.setZGyroOffset(24);   //-85
-    mpu.setZAccelOffset(812); // 1788 // 1688 factory default for my test chip
-#endif
 
     // supply your own gyro offsets here, scaled for min sensitivity
     mpu.setXGyroOffset(137);//220//[136,137] --> [0,2]  
@@ -104,33 +101,29 @@ void setup()
     mpu.setZGyroOffset(24);//-85
     mpu.setZAccelOffset(812);//1788 // 1688 factory default for my test chip
 
-
     // make sure it worked (returns 0 if so)
     if (devStatus == 0)
     {
         // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
+        Serial.println(("Enabling DMP..."));
         mpu.setDMPEnabled(true);
 
         // enable Arduino interrupt detection
-        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 18)..."));
-
-        attachInterrupt(18, dmpDataReady, RISING);
-
+        Serial.println(("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(digitalPinToInterrupt(2), dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        Serial.println(("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
 
         // get expected DMP packet size for later comparison
         packetSize = mpu.dmpGetFIFOPacketSize();
-
-
-        // setup PID
+        
+        //setup PID
         pid.SetMode(AUTOMATIC);
         pid.SetSampleTime(10);
-        pid.SetOutputLimits(-255, 255);
+        pid.SetOutputLimits(-255, 255);  
     }
     else
     {
@@ -138,26 +131,26 @@ void setup()
         // 1 = initial memory load failed
         // 2 = DMP configuration updates failed
         // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(("DMP Initialization failed (code "));
         Serial.print(devStatus);
-        Serial.println(F(")"));
+        Serial.println((")"));
     }
 }
 
+
 void loop()
 {
-#if 1
     // if programming failed, don't try to do anything
-    if (!dmpReady)
-        return;
+    if (!dmpReady) return;
 
     // wait for MPU interrupt or extra packet(s) available
     while (!mpuInterrupt && fifoCount < packetSize)
     {
-        // no mpu data - performing PID calculations and output to motors
-
+        //no mpu data - performing PID calculations and output to motors
+        
         pid.Compute();
         motorController.move(output, MIN_ABS_SPEED);
+        
     }
 
     // reset interrupt flag and get INT_STATUS byte
@@ -172,19 +165,18 @@ void loop()
     {
         // reset so we can continue cleanly
         mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
+        Serial.println(("FIFO overflow!"));
 
-        // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
     }
     else if (mpuIntStatus & 0x02)
     {
         // wait for correct available data length, should be a VERY short wait
-        while (fifoCount < packetSize)
-            fifoCount = mpu.getFIFOCount();
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
 
         // read a packet from FIFO
         mpu.getFIFOBytes(fifoBuffer, packetSize);
-
+        
         // track FIFO count here in case there is > 1 packet available
         // (this lets us immediately read more without waiting for an interrupt)
         fifoCount -= packetSize;
@@ -192,43 +184,14 @@ void loop()
         mpu.dmpGetQuaternion(&q, fifoBuffer);
         mpu.dmpGetGravity(&gravity, &q);
         mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-#if LOG_INPUT
-        Serial.print("ypr\t");
-        Serial.print(ypr[0] * 180 / PI);
-        Serial.print("\t");
-        Serial.print(ypr[1] * 180 / PI);
-        Serial.print("\t");
-        Serial.println(ypr[2] * 180 / PI);
-#endif
-        input = ypr[1] * 180 / PI + 180;
-    }
-
-#endif
+        #ifdef LOG_INPUT
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/PI);
+        #endif
+        input = ypr[1] * 180/PI + 180;
+   }
 }
-#endif
-
-#if 0
-MPU6050 mpu;
-
-int16_t gyroX, gyroRate;
-float gyroAngle=0;
-unsigned long currTime, prevTime=0, loopTime;
-
-void setup() {  
-  Wire.begin();
-  mpu.initialize();
-  Serial.begin(9600);
-}
-
-void loop() {
-  currTime = millis();
-  loopTime = currTime - prevTime;
-  prevTime = currTime;
-  
-  gyroX = mpu.getRotationX();
-  gyroRate = map(gyroX, -32768, 32767, -250, 250);
-  gyroAngle = gyroAngle + (float)gyroRate*loopTime/1000;
-  
-  Serial.println(gyroAngle);
-}
-#endif
