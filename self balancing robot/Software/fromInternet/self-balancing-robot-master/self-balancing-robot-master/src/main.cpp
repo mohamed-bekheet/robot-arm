b512a1dca8432ba6726b9540bec17e01b09c2a6a
m-maker2023 <86668363+m-maker2023@users.noreply.github.com>
2021-11-08 23:40:53 +0200

    UPDATE NEW FOLDERS

--- self balancing robot/Software/fromInternet/self-balancing-robot-master/self-balancing-robot-master/src/main.cpp
+++ self balancing robot/Software/fromInternet/self-balancing-robot-master/self-balancing-robot-master/src/main.cpp
@@ -563,13 +563,13 @@ ISR(PCINT2_vect)
 }
 #endif
 
-#if 1
+#if 0
 #include <Arduino.h>
 #include "BasicStepperDriver.h"
 
 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
 #define MOTOR_STEPS 200
-#define RPM 300
+#define RPM 700
 
 // Since microstepping is set externally, make sure this matches the selected mode
 // If it doesn't, the motor will move at a different RPM than chosen
@@ -617,11 +617,11 @@ void loop() {
 
 #endif
 
-#if 0
+#if 1
 #include <Arduino.h>// defines pins numbers
 
-const int stepPin = 3; 
-const int dirPin = 4; 
+const int stepPin = 9; 
+const int dirPin = 8; 
  
 void setup() {
   // Sets the two pins as Outputs
@@ -634,21 +634,23 @@ void loop() {
   // Makes 200 pulses for making one full cycle rotation
   for(int x = 0; x < 200; x++) {
     digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(10); 
+    delayMicroseconds(500); 
     digitalWrite(stepPin,LOW); 
     delayMicroseconds(500); 
   }
+  /*
   delay(1000); // One second delay
   
   digitalWrite(dirPin,LOW); //Changes the rotations direction
   // Makes 400 pulses for making two full cycle rotation
-  for(int x = 0; x < 6400; x++) {
+  for(int x = 0; x < 200; x++) {
     digitalWrite(stepPin,HIGH);
     delayMicroseconds(500);
     digitalWrite(stepPin,LOW);
     delayMicroseconds(500);
   }
   delay(1000);
+  */
 }
 #endif
 
